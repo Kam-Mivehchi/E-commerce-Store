f49d3439735ddf168197a32e2e662d671dba50a8
Kamyar Mivehchi <kammivehchi@gmail.com>
2022-03-09 18:55:46 -0800

    added foreign keys to belongs to associations

--- models/index.js
+++ models/index.js
@@ -6,19 +6,23 @@ const ProductTags = require('./ProductTag')
 
 
 //product belongs to a category
-Products.belongsTo(Categories,)
+Products.belongsTo(Categories, {
+    foreignKey: 'category_id',
+})
 //category has many product models
-Categories.hasMany(Products,);
+Categories.hasMany(Products, {
+    foreignKey: 'category_id',
+});
 
 //product belongs to many Tags
 Products.belongsToMany(Tag, {
     through: ProductTags,
-
+    foreignKey: 'product_id',
 });
 //tag belongs to many Products
 Tag.belongsToMany(Products, {
     through: ProductTags,
-
+    foreignKey: 'tag_id',
 });
 
 
